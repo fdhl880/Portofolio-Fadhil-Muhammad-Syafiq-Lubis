b6b8a7cb2ea4ee078d33732a3cc2490a87a82241
Fadhil Muhammad Syafiq <Fadhilsyafiq90@gmail.com>
2026-04-10 18:28:20 +0700

    chore: full architectural synchronization with Radit project format (Next 16, React 19, src dir)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
-  output: 'standalone',
   images: {
     unoptimized: true,
   },
-  reactStrictMode: true,
 };
 
 export default nextConfig;
