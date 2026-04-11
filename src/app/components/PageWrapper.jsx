3a96a863530338c98bef747524c038860bf9a17d
Fadhil Muhammad Syafiq <Fadhilsyafiq90@gmail.com>
2026-04-11 22:42:22 +0700

    feat: integrated AtelierDeconstruction and removed LuxuryEditorial section

--- src/app/components/PageWrapper.jsx
+++ src/app/components/PageWrapper.jsx
@@ -29,7 +29,7 @@ const Chronology = dynamic(() => import('./sections/RoadmapSection'), { ssr: fal
 const Vision = dynamic(() => import('./sections/VisionSection'), { ssr: false });
 const Discovery = dynamic(() => import('./sections/DiscoverySection'), { ssr: false });
 const NeuralCoreBase = dynamic(() => import('./ui/NeuralCore'), { ssr: false });
-const LuxuryEditorial = dynamic(() => import('./sections/LuxuryEditorial'), { ssr: false });
+const AtelierDeconstruction = dynamic(() => import('./sections/AtelierDeconstruction'), { ssr: false });
 const NeuralCore = dynamic(() => import('./ui/NeuralCore'), { ssr: false });
 
 export default function PageWrapper() {
@@ -81,8 +81,8 @@ export default function PageWrapper() {
           {/* New Hall of Mentors - The Giants */}
           <GiantsSection />
 
-          {/* New Luxury Editorial Exhibition */}
-          <LuxuryEditorial />
+          {/* New Deconstruction Core (3D Mode ONLY) */}
+          {mode === 'atelier' && <AtelierDeconstruction />}
 
           {/* Foundation - The Heritage Path */}
           <HeritageSection />
